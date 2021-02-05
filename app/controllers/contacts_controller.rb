8805ace14b5f2e53c461565133f1a9953b45cfb8
yokotamaki <ymaki0626@gmail.com>
2021-02-05 04:02:27 +0000

    お問い合わせ入力フォームの作成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
+  end
+
+  def create
+    contact = Contact.new(contact_params)
+    contact.save
+    redirect_to success_contacts_path
   end
 
   def success
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:title, :body, :name, :email)
   end
 end
