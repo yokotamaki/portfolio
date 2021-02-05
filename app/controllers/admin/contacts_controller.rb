8b1387093f4345de1fc06467cf5f1783b3f47e36
yokotamaki <ymaki0626@gmail.com>
2021-02-05 10:20:06 +0000

    管理者側 お問い合わせ一覧・詳細画面の作成

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,7 +1,21 @@
 class Admin::ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
+
+  def update
+    contact = Contact.find(params[:id])
+    contact.update(contact_params)
+    redirect_to request.referer
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:status)
+  end
+
 end
