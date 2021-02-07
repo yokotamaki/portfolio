b94a3d1adceef05241f868899b17a6fd5510d8ab
yokotamaki <ymaki0626@gmail.com>
2021-02-07 11:54:19 +0000

    管理者側 予約一覧・詳細画面の作成

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,7 +1,10 @@
 class Admin::ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
+    @guest = @reservation.guest
   end
 end
