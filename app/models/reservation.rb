9b5a2ac046bad9efc6835cbc7ff48ea7f1040b63
yokotamaki <ymaki0626@gmail.com>
2021-02-10 03:11:36 +0000

    日付の選択（バリデーションの設定）

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,21 @@
 class Reservation < ApplicationRecord
-  belongs_to :room, optional: true
+  belongs_to :room
   belongs_to :guest
+
+  # start_dateに対するバリデーション
+  validate :not_before_today
+  # end_dateに対するバリデーション
+  validate :not_before_start_date
+
+  def not_before_today
+    if start_date.nil? || start_date < Date.today
+      errors.add(:start_date, '過去の日付は選択できません！')
+    end
+  end
+
+  def not_before_start_date
+    if end_date.nil? || end_date != start_date + 1
+      errors.add(:end_date, '到着日の翌日しか選択できません！')
+    end
+  end
 end
