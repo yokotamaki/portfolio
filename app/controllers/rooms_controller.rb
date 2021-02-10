422cb4cdba644af58f2cc3aad61ef7d026bae2bb
yokotamaki <ymaki0626@gmail.com>
2021-02-10 11:30:15 +0000

    客室一覧ページ レイアウト(プッシュ漏れ)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,8 @@
 class RoomsController < ApplicationController
   def index
-    @rooms = Room.all
+    # @rooms = Room.all
+    @room1 = Room.find(1)
+    @room2 = Room.find(2)
+    @room3 = Room.find(3)
   end
 end
