8c357271746f44f47d9fe25e78cbaae7ba115e48
yokotamaki <ymaki0626@gmail.com>
2021-02-18 06:42:26 +0000

    情報入力画面（バリデーション修正・追加）

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,12 +1,18 @@
 class Guest < ApplicationRecord
   has_one :reservation, dependent: :destroy
 
-  validates :name, presence: { message: "名前を入力してください" }
-  validates :name_kana, presence: { message: "フリガナを入力してください" }
-  validates :zipcode, presence: { message: "郵便番号を入力してください" }
+  # バリデーションの設定
+  validates :name, presence: { message: "名前を入力してください" },
+                   length: { in: 2..10 , message: "２文字以上で入力してください"}
+  validates :name_kana, presence: { message: "フリガナを入力してください" },
+                        format: { with: /\p{katakana}/ , message: "フリガナは全角カタカナで入力してください"}
+  validates :birthday, presence: { message: "生年月日をを選択してください" }
+  validates :zipcode, presence: { message: "郵便番号を入力してください" },
+                      format: { with: /\A\d{7}\z/, message: "郵便番号は７桁で入力してください (ハイフンなし)"}
   validates :address, presence: { message: "住所を入力してください" }
   validates :phone_number, presence: { message: "電話番号を入力してください" }
   validates :email, presence: { message: "メールアドレスを入力してください" }
+
 
   attribute :start_date
   attribute :end_date
