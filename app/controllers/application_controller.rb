29d6e9b9d8b6bca1cba7d53bfecd1b35ff25760e
yokotamaki <ymaki0626@gmail.com>
2021-02-25 06:37:41 +0000

    ログイン・ログアウト後の遷移先の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
-  # def after_sign_in_path_for(resource)
-  #   admin_reservations_path(resource)
-  # end
-  # def after_sign_out_path_for(resource)
-  #   root_path(resource)
-  # end
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_reservations_path
+    when Customer
+      mypage_customer_path(current_customer)
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    elsif resource == :customer
+      root_path
+    end
+  end
 end
