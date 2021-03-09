2d2ff01a0696c7edb5ce7401f2392770e5e1fdbf
yokotamaki <ymaki0626@gmail.com>
2021-03-09 05:00:02 +0000

    管理者側検索（params→ストロングパラメータ変更）

--- app/controllers/admin/searchs_controller.rb
+++ app/controllers/admin/searchs_controller.rb
@@ -1,11 +1,17 @@
 class Admin::SearchsController < ApplicationController
   def search
-    @keyword = params[:keyword]
-    @model = params[:model]
+    @keyword = search_params[:keyword]
+    @model = search_params[:model]
     if @model == "reservation"
       @reservation = Reservation.search(@keyword)
     elsif @model == "contact"
       @contact = Contact.search(@keyword)
     end
   end
+
+  private
+    def search_params
+    params.fetch(:search, {}).permit(:keyword, :model)
+  end
+
 end
